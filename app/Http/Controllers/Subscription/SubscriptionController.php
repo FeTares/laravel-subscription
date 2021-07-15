d7ffb49ab9b0506bfd6c080e0e7384736939d97e
Felipe Tavares <felipe.amtavares1@gmail.com>
2021-07-15 19:57:48 -0300

    Create Controller and routes and views checkout

---
+++ app/Http/Controllers/Subscription/SubscriptionController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Subscription;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SubscriptionController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view('subscriptions.index');
+    }
+
+    public function store(Request $request)
+    {
+        $request->user()
+            ->newSubscription('default', 'price_1JDd5VLsbD1rSjyAJcLAB5PI')
+            ->create($request->token);
+
+        return redirect()->route('subscriptions.premium');
+    }
+
+    public function premium()
+    {
+        return view('subscriptions.premium');
+    }
+}
