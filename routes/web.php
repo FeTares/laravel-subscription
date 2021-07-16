969ad10fdd74f84c92f557e9a755ca8dc7659e3a
Felipe Tavares <felipe.amtavares1@gmail.com>
2021-07-15 22:02:12 -0300

    Create Form Checkout

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('subscriptions/checkout', [SubscriptionController::class, 'index'])->name('subscriptions.checkout');
-Route::get('subscriptions/checkout', [SubscriptionController::class, 'premium'])->name('subscriptions.premium');
+Route::get('subscriptions/premium', [SubscriptionController::class, 'premium'])->name('subscriptions.premium');
 
 Route::get('/', function () {
     return view('welcome');
