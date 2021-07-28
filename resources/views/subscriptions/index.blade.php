2c433a8df7723b47c1f81962ea569a453f6b496d
Felipe Tavares <felipe.amtavares1@gmail.com>
2021-07-28 11:00:02 -0300

    Validation Checkout Stripe

--- resources/views/subscriptions/index.blade.php
+++ resources/views/subscriptions/index.blade.php
@@ -9,6 +9,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+
+                    <div id="show-errors" style="display: none;" class="mt-2 text-sm text-red-600"></div>
+
                     <p>Assinar o: {{ $plan->name }}</p>
                     <form action="{{ route('subscriptions.store') }}" method="post" id="form">
                         @csrf
@@ -43,11 +46,21 @@ console.log(cardElement);
 // subscription payment
 const form = document.getElementById('form')
 const cardHolderName = document.getElementById('card-holder-name')
-const cardButtom = document.getElementById('card-buttom')
-const clientSecret = cardButtom.dataset.secret
+const cardButton = document.getElementById('card-buttom')
+const clientSecret = cardButton.dataset.secret
+
+const showErrors = document.getElementById('show-errors')
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault()
+
+    // Disable button
+    cardButton.classList.add('cursor-not-allowed')
+    cardButton.firstChild.data = 'Validando'
+
+    // Reset errors
+    showErrors.innerText = ''
+    showErrors.style.display = 'none'
 
     const { setupIntent, error } = await stripe.confirmCardSetup(
         clientSecret, {
@@ -62,8 +75,13 @@ form.addEventListener('submit', async (e) => {
     );
 
     if (error) {
-        alert('Erro')
         console.log(error)
+
+        showErrors.style.display = 'block'
+        showErrors.innerText = (error.type == 'validation_error') ? error.message : 'Dados inválidos, verifique novamente'
+
+        cardButton.classList.remove('cursor-not-allowed')
+        cardButton.firstChild.data = 'Enviar'
 
         return;
     }
@@ -77,3 +95,24 @@ form.addEventListener('submit', async (e) => {
     form.submit()
 })
 </script>
+
+<style>
+    .StripeElement {
+        background-color: white;
+        padding: 8px 12px;
+        border-radius: 4px;
+        border: 1px solid transparent;
+        box-shadow: 0 1px 3px 0 #e6ebf1;
+        -webkit-transition: box-shadow 150ms ease;
+        transition: box-shadow 150ms ease;
+    }
+    .StripeElement--focus {
+        box-shadow: 0 1px 3px 0 #cfd7df;
+    }
+    .StripeElement--invalid {
+        border-color: #fa755a;
+    }
+    .StripeElement--webkit-autofill {
+        background-color: #fefde5 !important;
+    }
+</style>
